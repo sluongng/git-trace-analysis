3bd7a1650bed68595dc5931c786468dc8a7f09bf
Son Luong Ngoc <son.luong@booking.com>
2021-05-09 18:49:51 +0200

    add serde

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use tokio::net::UnixDatagram;
 use std::io;
+use tokio::net::UnixDatagram;
+
+mod event;
+pub use self::event::Event;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -15,8 +18,7 @@ async fn main() -> io::Result<()> {
         match socket.try_recv(&mut data[..]) {
             Ok(size) => {
                 let dgram = &data[..size];
-                let event = String::from_utf8(dgram.to_vec()).unwrap();
-                handle_event(event).await;
+                handle_event(dgram).await;
             }
 
             // False-positive, continue
@@ -28,18 +30,26 @@ async fn main() -> io::Result<()> {
     }
 }
 
-async fn handle_event(event: String) {
-    println!("{}", event);
+async fn handle_event(event: &[u8]) {
+    let e: Event = match serde_json::from_slice(event) {
+        Err(_e) => return,
+        Ok(v) => v,
+    };
+
+    println!("{}", e.sid);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use tokio_test::block_on;
 
     #[test]
     fn should_handle_event() {
-        tokio_test::block_on(
-            handle_event(String::from("blah"))
+        block_on(
+            handle_event(
+                br#"{"event":"atexit","sid":"20210509T164222.307807Z-Hf8bd5587-P00013e6d","thread":"main","time":"2021-05-09T16:42:31.768234Z","file":"trace2/tr2_tgt_event.c","line":201,"t_abs":9.461512,"code":0}"#
+            )
         );
     }
 }
